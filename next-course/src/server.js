fd6f0e1e506e8dd792940dda58813565e2afea27
marvin <50601920+pedrow42@users.noreply.github.com>
2020-08-27 17:47:44 -0300

    back-end#2

--- next-course/src/server.js
+++ next-course/src/server.js
@@ -1,7 +1,7 @@
 const proffys = [
     {name: "Diego Fernandes", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "https://api.whatsapp.com/send?phone=5553981090277", 
+    whatsapp: "53981090277", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
     cost: "20", 
@@ -11,7 +11,7 @@ const proffys = [
     
     {name: "Pedro Lopes", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "https://api.whatsapp.com/send?phone=5553981090277", 
+    whatsapp: "53999037982", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
     cost: "20", 
@@ -19,32 +19,52 @@ const proffys = [
     time_from: [], 
     time_to: []}
 ]
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
 function pageLanding(req, res){
     return res.render("site.html")
 }
 function pageStudy(req, res){
-    return res.render("study.html")
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
 function pageGiveClasses(req, res){
     return res.render("give-classes.html")
 }
-
+//servidor
 const express = require('express')
 const server = express()
-
+//configurando nunjucks(template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views',{
     express: server,
     noCache: true,
 })
 
-
-
-
+//inicio e configuração do servidor
 server
 .use(express.static("public")) // configurar arquivos estaticos (css, scripts, imgs)
 .get("/", pageLanding) // rotas da aplicação
 .get("/study", pageStudy) // rotas da aplicação
 .get("/give-classes", pageGiveClasses) // rotas da aplicação
     
-.listen(5550)
+.listen(5550) //start no servidor
